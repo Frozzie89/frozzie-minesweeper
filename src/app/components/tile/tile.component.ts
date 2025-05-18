cc875c9faff337f11889aa600c4bffe6acd2776c
Frozzie89 <froztor85@gmail.com>
2025-05-18 18:13:16 +0200

    displaying tile reveal

--- src/app/components/tile/tile.component.ts
+++ src/app/components/tile/tile.component.ts
@@ -3,18 +3,21 @@ import { Tile } from '../../classes/tile';
 import { FreeTile } from '../../classes/free-tile';
 import { BombTile } from '../../classes/bomb-tile';
 import { MatCardModule } from '@angular/material/card';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBomb } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-tile',
     standalone: true,
-    imports: [MatCardModule, NgClass],
+    imports: [MatCardModule, NgClass, NgIf, FontAwesomeModule],
     templateUrl: './tile.component.html',
     styleUrl: './tile.component.scss'
 })
 export class TileComponent implements OnInit {
 
     tile!: Tile
+    faBomb = faBomb
 
     ngOnInit(): void {
         if (Math.floor(Math.random() * 2) == 0) {
@@ -22,6 +25,10 @@ export class TileComponent implements OnInit {
         } else {
             this.tile = new BombTile(0, 0)
         }
+    }
+
+    isBomb(): boolean {
+        return this.tile instanceof BombTile
     }
 
     clickTile(): void {
