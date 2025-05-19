1781167ee56bc8d54ef1741e620e73b4e62b6e2f
Frozzie89 <froztor85@gmail.com>
2025-05-19 12:14:18 +0200

    tests for tile-number-display pipe

--- src/app/pipes/tile-number-display.pipe.spec.ts
+++ src/app/pipes/tile-number-display.pipe.spec.ts
@@ -1,8 +1,18 @@
 import { TileNumberDisplayPipe } from './tile-number-display.pipe';
 
 describe('TileNumberDisplayPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TileNumberDisplayPipe();
-    expect(pipe).toBeTruthy();
-  });
+    it('create an instance', () => {
+        const pipe = new TileNumberDisplayPipe();
+        expect(pipe).toBeTruthy();
+    });
+
+    it('0 becomes an empty string', () => {
+        const pipe = new TileNumberDisplayPipe();
+        expect(pipe.transform(0)).toBe('');
+    });
+
+    it('any number other than 0 becomes string of that number', () => {
+        const pipe = new TileNumberDisplayPipe();
+        expect(pipe.transform(1)).toBe('1');
+    });
 });
