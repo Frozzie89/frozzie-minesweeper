3ed00466edf210bdf286242be97db409d87a94d7
Frozzie89 <froztor85@gmail.com>
2025-05-20 11:55:24 +0200

    title is displayed on the top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TileMatrixComponent } from './components/tile-matrix/tile-matrix.component';
 import { BombCounterComponent } from "./components/bomb-counter/bomb-counter.component";
+import { TitleComponent } from "./components/title/title.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, TileMatrixComponent, BombCounterComponent],
+    imports: [RouterOutlet, TileMatrixComponent, BombCounterComponent, TitleComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
